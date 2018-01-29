abd7cb0a69fced4d55178b1e30f6b72520c340a8
Thomas Blanquet <thomasblanquet@Thomass-MacBook-Pro.local>
2018-01-29 22:47:24 +0100

    comments

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ size = (0, 0)
 def errorQuit(message):
     print("Error: " + message, file=sys.stderr)
     sys.exit(1)
-    
+
+#Open the image
 def loadImage(image):
     print("Loading image...")
     global originalPic
     originalPic = Image.open(image)
     print("Loaded !")
-    
+
+#Convert image to nparray of pixels (RGB)
 def imageToList():
     print("Putting image to array of pixels...")
     global pixels
@@ -26,7 +28,8 @@ def imageToList():
     pixels = np.array(originalPic.getdata()).reshape(originalPic.size[1], originalPic.size[0], 3)
     size = (originalPic.size[1], originalPic.size[0])
     print ("Done !")
-    
+
+#By using a formula, create a black and white image using white for pixels brighter than the threshold and black otherwise
 def createLuminanceImage(name):
     print("Creating luminance filter...")
     global pixelsLuminanceImage
@@ -44,6 +47,7 @@ def createLuminanceImage(name):
     image.save(name, "JPEG", quality=80, optimize=True, progressive=True)
     print("Saved !")
 
+#Create and fill an 2D array corresponding to pixels's luminance
 def initLuminanceTab():
     global size
     global pixelsLuminance
